29dd144cf072d89ac172fb98e3a9bcff8738e52f
akihito7 <akihitopro7@gmail.com>
2023-09-22 18:35:11 -0300

    adicionei as pastas util e configs

--- backend/src/database/migrations/20230922211934_create_table_events.js
+++ backend/src/database/migrations/20230922211934_create_table_events.js
@@ -1,15 +1,11 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.up = function(knex) {
+exports.up = knex => knex.schema.createTable("events", table => {
+    table.increments("id");
+    table.text("name").notNullable();
+    table.text("description").notNullable();
+    table.text("modality").notNullable();
+    table.text("date").notNullable();
+    table.timestamp("created_at").default(knex.fn.now());
+    table.timestamp("updated_at").default(knex.fn.now());
+  });
   
-};
-
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.down = function(knex) {
-  
-};
+  exports.down = knex => knex.schema.dropTable("events");
